57e956e0a8b99d98af5f600703483e8c3558ba24
[dhhan] <[dhhan@enliple.com]>
2024-10-10 12:49:30 +0900

    오타수정

--- page/report/report.php
+++ page/report/report.php
@@ -506,7 +506,7 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
                     } else if (checkDate[1] === 6) {
                         keyword.classList.add('sun');
                     }
-                } else if (earchType === 'MONTH') {
+                } else if (searchType === 'MONTH') {
                     const checkDate = formatAndCheckDate(item.keyWordName);
                     keywordText = checkDate[0];
                 } else {
