d0c745f56bcaac3555105a3ed36ace1cbeefe3b9
[dhhan] <[dhhan@enliple.com]>
2024-10-14 12:15:15 +0900

    상세보기 모달 조작, css 수정

--- page/report/reportModal.php
+++ page/report/reportModal.php
@@ -3,77 +3,73 @@
 <style>
  
 </style>
-<div id="myModal" class="modal">
-  <div class="modal-content" id="modal-content">
-    <span id="close-modal" style="cursor:pointer;">&times;</span>
-    <div id="modal-body">
-          <div class="modal-content">
-          <section class="modal-sec_list">
-              <div class="modal-tableHeader">
-                  <div class="modal-tableTitle">
-                      <p>요약 리포트<span>일별</span></p>
-                  </div>
-                  <div class="modal-filterArea">
-                    <div class="modal-optionBox">
-                        <div>
-                            <div class="modal-calendarBox">
-                                <div class="modal-calendar">
-                                    <input type="text" id="modal-dateInput" name=dateInput" value="">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-selectBox">
-                        <select id="modal-size">
-                            <option value="10">10개씩 보기</option>
-                            <option value="20">20개씩 보기</option>
-                            <option value="40" selected>40개씩 보기</option>
-                            <option value="60">60개씩 보기</option>
-                            <option value="100">100개씩 보기</option>
-                        </select>
-                    </div>
-                  </div>
-              </div>
-              <div class="modal-tableArea">
-                  <div class="modal-tableBox modal-selectDay">
-                      <table>
-                          <thead id="modal-reportHead">
-                              <tr>
-                                  <th id="modal-searchTypeTitle" class="modal-sortDown">날짜</th>
-                                  <th class="modal-sort">노출수</th>
-                                  <th class="modal-sort">클릭수</th>
-                                  <th class="modal-sort">건수</th>
-                                  <th class="modal-sort">전환율</th>
-                                  <th class="modal-sort">구매액</th>
-                                  <th class="modal-sort">커미션 매출</th>
-                                  <th class="modal-sort">커미션 이익</th>
-                              </tr>
-                          </thead>
-                          <tbody id="modal-reportData">
-
-                          </tbody>
-                      </table>
-                  </div>
-
-                  <div class="modal-tableDataNone" style="display: none;">
-                      <div>
-                          <p>내용이 없습니다. </p>
+<div class="modal" id="myModal">
+  <div class="modal-background" onclick="closeModal()"></div>
+  <div class="modal-content">
+    <section class="modal-sec_list">
+      <div class="modal-tableHeader">
+        <div class="modal-headerTop">
+          <div class="modal-tableTitle">
+              <p>요약 리포트<span>일별</span></p>
+          </div>
+          <span class="close-modal" onclick="closeModal()">&times;</span>
+        </div>
+        <div class="modal-filterArea">
+          <div class="modal-optionBox">
+              <div>
+                  <div class="modal-calendarBox">
+                      <div class="modal-calendar">
+                          <input type="text" id="modal-dateInput" name=dateInput" value="">
                       </div>
                   </div>
-                  <div class="modal-paging">
-                      <ul></ul>
-                  </div>
               </div>
-          </section>
+          </div>
+          <div class="modal-selectBox">
+              <select id="modal-size">
+                  <option value="10">10개씩 보기</option>
+                  <option value="20">20개씩 보기</option>
+                  <option value="40" selected>40개씩 보기</option>
+                  <option value="60">60개씩 보기</option>
+                  <option value="100">100개씩 보기</option>
+              </select>
+          </div>
+        </div>
       </div>
-    </div>
+      <div class="modal-tableArea">
+        <div class="modal-tableBox modal-selectDay">
+            <table>
+                <thead id="modal-reportHead">
+                    <tr>
+                        <th id="modal-searchTypeTitle" class="modal-sortDown">날짜</th>
+                        <th class="modal-sort">노출수</th>
+                        <th class="modal-sort">클릭수</th>
+                        <th class="modal-sort">건수</th>
+                        <th class="modal-sort">전환율</th>
+                        <th class="modal-sort">구매액</th>
+                        <th class="modal-sort">커미션 매출</th>
+                        <th class="modal-sort">커미션 이익</th>
+                    </tr>
+                </thead>
+                <tbody id="modal-reportData">
+
+                </tbody>
+            </table>
+        </div>
+        <div class="modal-tableDataNone" style="display: none;">
+          <div>
+              <p>내용이 없습니다. </p>
+          </div>
+        </div>
+        <div class="modal-paging">
+          <ul></ul>
+        </div>
+      </div>
+    </section>
   </div>
 </div>
 <script>
   // 모달 열기
   const modal = document.getElementById("myModal");
-  const modalBody = document.getElementById("modal-body");
-  const closeModal = document.getElementById("close-modal");
 
    // API 응답 처리 및 데이터 렌더링
    function modalHandleSuccessResponse(data, size, page, modalSearchType) {
@@ -114,16 +110,8 @@
     console.log('모달데이터 요청');
   }
 
-  // 모달 닫기
-  closeModal.onclick = function() {
+  function closeModal(){
     modal.style.display = "none";
-  }
-
-  // 모달 외부를 클릭하면 모달 닫기
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
   }
 
   // 상세보기 모달 닫을때 데이터 렌더링 초기화
