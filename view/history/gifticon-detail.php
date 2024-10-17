f803654701409ce175eeb7f442a518ee27266051
[dhhan] <[dhhan@enliple.com]>
2024-10-17 21:09:16 +0900

    기프티콘 당첨내역리스트, 상세내역

--- view/history/gifticon-detail.php
+++ view/history/gifticon-detail.php
@@ -1,3 +1,13 @@
+<?php
+$object = $_REQUEST['object'] ?? null; // null로 기본값 설정
+if (!$object) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    exit;
+}
+
+// 여기서 object를 JSON으로 인코딩
+$objectJson = json_encode($object);
+?>
 <!DOCTYPE html>
 <html lang="ko">
 
@@ -40,11 +50,11 @@
                     <div class="date-info-box">
                         <div class="date-box date-box1">
                             <p class="text">당첨일자</p>
-                            <p class="date">2024.09.30</p>
+                            <p class="date"></p>
                         </div>
                         <div class="date-box date-box2">
                             <p class="text">유효기간</p>
-                            <p class="date">2024.10.30</p>
+                            <p class="date"></p>
                         </div>
                         <p class="text">*교환이나 환불, 유효기간은 연장이 불가합니다.</p>
                     </div>
@@ -70,48 +80,9 @@
                         </ul>
                     </div>
                 </div>
-                <!-- 기프티콘 상세 지급예정 -->
-                <div class="giftcon-info-wrap type2" style="margin-bottom: 20px;">
-                    <div class="goods-info-box">
-                        <div class="goods-img" style="background-image: url(/view/images/test/스타벅스상품.png);"></div>
-                        <div class="logo-box">
-                            <div class="logo" style="background-image: url(/view/images/test/스타벅스로고.png);"></div>
-                            <p class="logo-title">스타벅스</p>
-                        </div>
-                        <p class="title">아이스 카페 아메리카노 T</p>
-                    </div>
-                    <div class="date-info-box">
-                        <div class="date-box date-box1">
-                            <p class="text">당첨일자</p>
-                            <p class="date">2024.09.30</p>
-                        </div>
-                        <div class="date-box date-box2">
-                            <p class="text">유효기간</p>
-                            <p class="date">2024.10.30</p>
-                        </div>
-                        <p class="text">*교환이나 환불, 유효기간은 연장이 불가합니다.</p>
-                    </div>
-                    <p class="title">유의사항</p>
-                    <div class="gray-box">
-                        <p class="title">상품고시정보</p>
-                        <ul>
-                            <li>발행자 : 주식회사 인라이플</li>
-                            <li>교환권 공급자 : (주)OOO마케팅</li>
-                        </ul>
-                        <p class="title">유효기간</p>
-                        <ul>
-                            <li>각 상품 상세페이지 내 유효기간 확인</li>
-                        </ul>
-                        <p class="title">이용조건(유효기간 경과시 보상 기준 포함)</p>
-                        <ul>
-                            <li>본상품의 매장 별 판매가격이 상이 할 수 있으며, 일부 매장에서는 추가금액 결제 후 교환 가능합니다.</li>
-                            <li>물품교환권은 구매일로부터 180일 이내, 금액 교환권은 270일 이내 2회 기간연장 가능합니다.</li>
-                            <li>상품에 따라 기간 연장이 불가한 상품이 있으니, 상품 설명을 참고하시기 바랍니다.</li>
-                        </ul>
-                    </div>
-                </div>
+
                 <!-- 기프티콘 상세 사용완료 -->
-                <div class="giftcon-info-wrap type3">
+                <!-- <div class="giftcon-info-wrap type3">
                     <div class="goods-info-box">
                         <div class="goods-img" style="background-image: url(/view/images/test/스타벅스상품.png);"></div>
                         <div class="logo-box">
@@ -153,9 +124,9 @@
                             <li>상품에 따라 기간 연장이 불가한 상품이 있으니, 상품 설명을 참고하시기 바랍니다.</li>
                         </ul>
                     </div>
-                </div>
+                </div> -->
                 <!-- 기프티콘 상세 사용만료 -->
-                <div class="giftcon-info-wrap type3">
+                <!-- <div class="giftcon-info-wrap type3">
                     <div class="goods-info-box">
                         <div class="goods-img" style="background-image: url(/view/images/test/스타벅스상품.png);"></div>
                         <div class="logo-box">
@@ -197,7 +168,7 @@
                             <li>상품에 따라 기간 연장이 불가한 상품이 있으니, 상품 설명을 참고하시기 바랍니다.</li>
                         </ul>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -207,15 +178,30 @@
 
 </html>
 <script>
-    function getGifticonDetail() {
+    const object = JSON.parse(<?= $objectJson ?>); // PHP에서 만든 JSON 문자열을 객체로 변환
+
+    $(function() {
+        renderGifticonDetail(object); // 함수 호출
+    });
+
+
+    function renderGifticonDetail(object) {
         try {
+            if (!object) {
+                alert('존재하지않는 기프티콘입니다.');
+                history.back();
+                return
+            }
+
+            const item = object;
+            console.log(item);
+            $('.giftcon-info-wrap').addClass(item.giftYn === 'N' ? '' : 'type3');
+
+            $('.date-box1 .date').append(formatDate(item.awardDay));
+            $('.date-box2 .date').append(formatDate(item.validDay));
 
         } catch (error) {
             alert(error);
         }
-    }
-
-    function renderGifticonDetail(data) {
-
     }
 </script>
