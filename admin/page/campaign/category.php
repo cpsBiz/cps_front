dd29541e3cbbdbcf9938381ea49a1f19579a945b
[dhhan] <[dhhan@enliple.com]>
2024-10-21 19:09:05 +0900

    캠페인 관리 - 캠페인 카테고리 - 카테고리 캠페인 관리 개발중

--- admin/page/campaign/category.php
+++ admin/page/campaign/category.php
@@ -3,7 +3,7 @@
 $tab = $_REQUEST['tab'];
 
 $page = (int)(isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
-$per = 10;
+$per = 50;
 $total_page = 0;
 $total = 0;
 ?>
@@ -123,14 +123,15 @@ $total = 0;
 
 
 													// 결과를 처리
+													$i = $page_int + 1;
 													while ($row = mysqli_fetch_assoc($result)) {
 														$categoryRank = $row['CATEGORY_RANK'];
 														$categoryName = $row['CATEGORY_NAME'];
 														$campaignCnt = $row['CAMPAIGN_CNT'];
 														$category = $row['CATEGORY'];
 												?>
-														<tr id="categoryList<?= $category; ?>">
-															<td><?= $categoryRank; ?></td>
+														<tr id="categoryList<?= $i; ?>">
+															<td><?= $i; ?></td>
 															<td><?= $categoryName; ?></td>
 															<td><?= $campaignCnt; ?></td>
 															<td>
@@ -144,6 +145,7 @@ $total = 0;
 															</td>
 														</tr>
 												<?
+														$i++;
 													}
 
 													mysqli_stmt_close($stmt);
@@ -153,14 +155,14 @@ $total = 0;
 										</table>
 									</div>
 									<? if ($total == 0) { ?>
-										<div class="tableDataNone">
+										<div class="categoryList tableDataNone">
 											<div>
 												<p>내용이 없습니다. </p>
 											</div>
 										</div>
 										<script>
 											$('#categoryList .tableBox').hide();
-											$('.tableDataNone').show();
+											$('.categoryList.tableDataNone').show();
 										</script>
 									<? } else { ?>
 										<div class="paging">
@@ -194,18 +196,36 @@ $total = 0;
 							</div>
 						<? } ?>
 						<!-- tab 2 > 카테고리 캠페인 관리  -->
-						<? if ($tab == 'campaign') { ?>
+						<? if ($tab == 'campaign') {
+
+							$paramCategory = $_REQUEST['category'];
+						?>
 							<div class="tabViewList show">
 								<div class="tableHeader">
 									<div class="tableTitle">
 										<p>카테고리 캠페인 관리</p>
 									</div>
 									<div class="selectBox">
-										<select name="" id="" class="category">
-											<option value="">카테고리 선택</option>
+										<select id="selectCategory" class="category" onchange="selectCategory()">
+											<?
+											$sql = "
+															SELECT 
+																CATEGORY, CATEGORY_NAME 
+															FROM CPS_CATEGORY 
+															ORDER BY CATEGORY_RANK ASC
+															";
+											$stmt = mysqli_stmt_init($con);
+											if (mysqli_stmt_prepare($stmt, $sql)) {
+												mysqli_stmt_execute($stmt);
+												$result = mysqli_stmt_get_result($stmt);
+												while ($row = mysqli_fetch_assoc($result)) {
+											?>
+													<option value="<?= $row['CATEGORY']; ?>" <? if ($paramCategory == $row['CATEGORY']) echo 'selected'; ?>><?= $row['CATEGORY_NAME']; ?></option>
+											<? }
+											} ?>
 										</select>
 										<select name="" id="">
-											<option value="">10개씩 보기</option>
+											<option value="">50개씩 보기</option>
 										</select>
 									</div>
 									<div class="buttonBox">
@@ -216,7 +236,7 @@ $total = 0;
 								</div>
 								<!--// 내용이 없을 때 tableWrap & tableAreaDataNone 함께 사용 -->
 								<!-- <div class="tableArea tableAreaDataNone"> -->
-								<div class="tableArea">
+								<div id="campaignList" class="tableArea">
 									<!-- table 스크롤 생기게 하고 싶을 때 tableBox 에 높이값(max-height) 추가-->
 									<div class="tableBox">
 										<table>
@@ -237,79 +257,149 @@ $total = 0;
 												</tr>
 											</thead>
 											<tbody>
-												<tr>
-													<td>1</td>
-													<td>쿠팡 </td>
-													<td>
-														<div class="buttonBox">
-															<button type="button" class="categoryChange">카테고리 변경</button>
-														</div>
-													</td>
-													<td>
-														<div class="checkBox">
-															<input type="checkbox" name="chk2" id="chk2_1">
-															<label for="chk2_1"></label>
-														</div>
-													</td>
-													<td>
-														<div class="buttonBox"><button type="button"
-																class="listChange">순위변경</button></div>
-													</td>
-												</tr>
-												<tr>
-													<td>2</td>
-													<td>G마켓</td>
-													<td>
-														<div class="buttonBox">
-															<button type="button" class="categoryChange">카테고리 변경</button>
-														</div>
-													</td>
-													<td>
-														<div class="checkBox">
-															<input type="checkbox" name="chk2" id="chk2_2">
-															<label for="chk2_2"></label>
-														</div>
-													</td>
-													<td>
-														<div class="buttonBox"><button type="button"
-																class="listChange">순위변경</button></div>
-													</td>
-												</tr>
-												<tr>
-													<td>&nbsp;</td>
-													<td>&nbsp;</td>
-													<td>&nbsp;</td>
-													<td>&nbsp;</td>
-													<td>&nbsp;</td>
-												</tr>
+												<?
+												$sql = "
+																SELECT 
+																	C.CATEGORY_NAME,
+																	A.CATEGORY,
+																	A.CAMPAIGN_RANK,
+																	B.CAMPAIGN_NAME
+																FROM CPS_CAMPAIGN_RANK A
+																JOIN CPS_CAMPAIGN B ON A.CAMPAIGN_NUM = B.CAMPAIGN_NUM
+																JOIN CPS_CATEGORY C ON C.CATEGORY = A.CATEGORY 
+																WHERE 
+																	A.CATEGORY = ?
+																GROUP BY A.CAMPAIGN_NUM
+																ORDER BY CAMPAIGN_RANK ASC
+																LIMIT ?, ?
+																";
+
+												$page_int = ($page - 1) * $per;
+
+												$stmt = mysqli_stmt_init($con);
+												if (mysqli_stmt_prepare($stmt, $sql)) {
+													mysqli_stmt_bind_param($stmt, 'sii', $paramCategory, $page_int, $per);
+													mysqli_stmt_execute($stmt);
+													$result = mysqli_stmt_get_result($stmt);
+
+													//페이징
+													$query = "SELECT FOUND_ROWS()";
+													$stmt = mysqli_prepare($con, $query);
+													mysqli_stmt_execute($stmt);
+													mysqli_stmt_bind_result($stmt, $num);
+													while (mysqli_stmt_fetch($stmt)) {
+														$total = $num;
+													}
+
+													if ($total % $per == 0) {
+														$total_page = (int)($total / $per);
+													} else {
+														$total_page = (int)($total / $per) + 1;
+													}
+
+
+													// 결과를 처리
+													$i = $page_int + 1;
+													while ($row = mysqli_fetch_assoc($result)) {
+														$campaignName = $row['CAMPAIGN_NAME'];
+
+												?>
+														<tr id="campaignList<?= $i; ?>">
+															<td><?= $i; ?></td>
+															<td><?= $campaignName; ?></td>
+															<td>
+																<div class="buttonBox">
+																	<button type="button" class="categoryChange">카테고리 변경</button>
+																</div>
+															</td>
+															<td>
+																<div class="checkBox">
+																	<input type="checkbox" name="chk2" id="chk2_1">
+																	<label for="chk2_1"></label>
+																</div>
+															</td>
+															<td>
+																<div class="buttonBox"><button type="button"
+																		class="listChange">순위변경</button></div>
+															</td>
+														</tr>
+
+														<!-- <tr id="categoryList<?= $category; ?>">
+															<td><?= $categoryRank; ?></td>
+															<td><?= $categoryName; ?></td>
+															<td><?= $campaignCnt; ?></td>
+															<td>
+																<div class="buttonBox">
+																	<button type="button" class="modify" title="수정" onclick="modifyCategory('<?= $category; ?>', '<?= $categoryName; ?>', <?= $categoryRank; ?>)">수정</button>
+																	<button type="button" class="delete" title="삭제" onclick="deleteCategory('<?= $category; ?>')">삭제</button>
+																</div>
+															</td>
+															<td>
+																<div class="buttonBox"><button type="button" class="listChange">순위변경</button></div>
+															</td>
+														</tr> -->
+												<?
+													}
+
+													mysqli_stmt_close($stmt);
+												}
+												?>
 											</tbody>
 										</table>
 									</div>
-									<div class="tableDataNone">
-										<div>
-											<p>내용이 없습니다. </p>
+									<? if ($total == 0) { ?>
+										<div class="campaignList tableDataNone">
+											<div>
+												<p>내용이 없습니다. </p>
+											</div>
 										</div>
-									</div>
-									<div class="paging">
-										<ul>
-											<!-- <li class="prev-list"><a href="javascript:pageLink(0);"></a></li> -->
-											<li class="prev"><a href="javascript:pageLink(0);"></a></li>
-											<li class="on"><a href="javascript:pageLink(1);">1</a></li>
-											<li><a href="javascript:pageLink(2);">2</a></li>
-											<li><a href="javascript:pageLink(3);">3</a></li>
-											<li><a href="javascript:pageLink(4);">4</a></li>
-											<li><a href="javascript:pageLink(5);">5</a></li>
-											<li><a href="javascript:pageLink(6);">6</a></li>
-											<li><a href="javascript:pageLink(7);">7</a></li>
-											<li><a href="javascript:pageLink(8);">8</a></li>
-											<li><a href="javascript:pageLink(9);">9</a></li>
-											<li><a href="javascript:pageLink(10);">10</a></li>
-											<li class="next"><a href="javascript:pageLink(10+1);"></a></li>
-											<!-- <li class="next-list"><a href="javascript:pageLink(10+1);"></a></li> -->
-										</ul>
-									</div>
+										<script>
+											$('#campaignList .tableBox').hide();
+											$('.campaignList.tableDataNone').show();
+										</script>
+									<? } else { ?>
+										<div class="paging">
+											<ul>
+												<!-- 이전 페이지 -->
+												<? if ($page > 1) { ?>
+													<li class="prev"><a href="javascript:pageLink(<?= $page - 1; ?>);"></a></li>
+												<? } else { ?>
+													<li class="prev disabled"><a></a></li>
+												<? } ?>
+
+												<!-- 페이지리스트 -->
+												<? for ($i = 1; $i <= $total_page; $i++) { ?>
+													<? if ($i == $page) { ?>
+														<li class="on"><a href="javascript:pageLink(<?= $i; ?>);"><?= $i; ?></a></li>
+													<? } else { ?>
+														<li><a href="javascript:pageLink(<?= $i; ?>);"><?= $i; ?></a></li>
+												<? }
+												} ?>
+
+												<!-- 다음페이지 -->
+												<? if ($page < $total_page) { ?>
+													<li class="next"><a href="javascript:pageLink(<?= $page + 1; ?>);"></a></li>
+												<? } else { ?>
+													<li class="next disabled"><a></a></li>
+												<? } ?>
+											</ul>
+										</div>
+									<? } ?>
 								</div>
 							</div>
+							<script>
+								function selectCategory() {
+									const category = document.getElementById('selectCategory').value;
+
+									const currentUrl = window.location.href;
+
+									const url = new URL(currentUrl);
+
+									url.searchParams.set('category', category);
+
+									window.location.href = url.toString();
+								}
+							</script>
 						<? } ?>
 						<? if ($tab == 'pop') { ?>
 							<!-- tab 3 > 인기 캠페인 관리  -->
@@ -446,6 +536,13 @@ $total = 0;
 		window.location.href = "?tab=<?= $tab; ?>&page=" + pageNumber; // 필요한 파라미터 추가
 	}
 </script>
-<? include_once $_SERVER['DOCUMENT_ROOT'] . "/admin/page/campaign/category-add.php"; ?>
-<? include_once $_SERVER['DOCUMENT_ROOT'] . "/admin/page/campaign/category-modify.php"; ?>
-<? include_once $_SERVER['DOCUMENT_ROOT'] . "/admin/page/campaign/category-delete.php"; ?>
+<?
+if ($tab == '' || $tab == 'category') {
+	include_once $_SERVER['DOCUMENT_ROOT'] . "/admin/page/campaign/category-add.php";
+	include_once $_SERVER['DOCUMENT_ROOT'] . "/admin/page/campaign/category-modify.php";
+	include_once $_SERVER['DOCUMENT_ROOT'] . "/admin/page/campaign/category-delete.php";
+}
+
+if ($tab == 'campaign') {
+}
+?>
