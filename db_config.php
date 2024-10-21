1209e5072eaeaa1eea4b5ba7b468269e95e0ae91
[dhhan] <[dhhan@enliple.com]>
2024-10-21 15:35:17 +0900

    디비연결

--- db_config.php
+++ db_config.php
@@ -1,11 +1,16 @@
 <?
-$host = '192.168.150.61:3306';
-$username = 'dhhan';
-$password = 'dhhan@enliple.com';
+// 데이터베이스 연결 정보
+$host = '192.168.101.156:3306';
+$username = 'CPS';
+$password = 'Emfla2017!@#';
 $database = 'CPS';
 
+// MySQL 데이터베이스 연결
 $con = mysqli_connect($host, $username, $password, $database);
 
+// 연결 확인
 if (!$con) {
   die('MySQL 연결 실패: ' . mysqli_connect_error());
+  header('Location:/500.php');
 }
+mysqli_set_charset($con, 'utf8');
--- header.php
+++ header.php
@@ -1,1 +1,3 @@
-<? include_once $_SERVER['DOCUMENT_ROOT'] . "db_config.php";
+<?
+include_once $_SERVER['DOCUMENT_ROOT'] . "/db_config.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/common.php";
