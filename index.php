ff530e60a69927f4d7e9ca3b160e6cdb008a2c9d
[dhhan] <[dhhan@enliple.com]>
2024-10-31 12:29:54 +0900

    테스트

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ if ($isTest) {
   $site = $_REQUEST['site'];
   $zoneId = $_REQUEST['zoneId'];
 
+  $dataCheck = 'userId : ' . $userId . ', adId :' . $adId . ', affliateId :' . $affliateId . ', site : ' . $site . ', zoneId :' . $zoneId;
+?>
+  <script>
+    alert('<?= $dataCheck; ?>')
+  </script>
+  <?
+  exit;
   if ($userId && $adId && $affliateId && $site && $zoneId) {
     session_start();
     $_SESSION['check_userId'] = $userId;
@@ -19,7 +26,7 @@ if ($isTest) {
 
     header('location:/view/index.php');
   } else {
-?>
+  ?>
     <script>
       alert('필수 값이 없습니다. 종료 후 다시 시도해 주세요.')
     </script>
