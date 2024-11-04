d7ce8b704e5d1b1c8b8d7880284220b6c7524df7
[dhhan] <[dhhan@enliple.com]>
2024-11-04 11:41:06 +0900

    리다이렉트 수정

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 session_start();
 $admin_login = $_SESSION['admin_login'];
 if ($admin_login !== true) {
-  header('Location:/admin/page/login.php');
+  header('Location:/page/login.php');
 } else {
-  header('Location:/admin/page/report/report.php');
+  header('Location:/page/report/report.php');
 }
