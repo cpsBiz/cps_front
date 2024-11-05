06b706542f61f39dd2a5dcd6d702f837411a51ee
[dhhan] <[dhhan@enliple.com]>
2024-11-05 10:58:10 +0900

    test

--- view/index.php
+++ view/index.php
@@ -1,3 +1,31 @@
+<? include_once $_SERVER['DOCUMENT_ROOT'] . '/isTest.php'; ?>
+<?
+if (!$isTest) {
+
+	$userId = $_REQUEST['userId'];
+	$adId = $_REQUEST['adId'];
+	$affliateId = $_REQUEST['affliateId'];
+	$site = $_REQUEST['site'];
+	$zoneId = $_REQUEST['zoneId'];
+?>
+	<?
+	if ($userId && $adId && $affliateId && $site && $zoneId) {
+		session_start();
+		$_SESSION['check_userId'] = $userId;
+		$_SESSION['check_adId'] = $adId;
+		$_SESSION['check_affliateId'] = $affliateId;
+		$_SESSION['check_site'] = $site;
+		$_SESSION['check_zoneId'] = $zoneId;
+	} else {
+	?>
+		<script>
+			alert('필수 값이 없습니다. 종료 후 다시 시도해 주세요.');
+		</script>
+<?
+		exit;
+	}
+}
+?>
 <? include_once $_SERVER['DOCUMENT_ROOT'] . "/header.php"; ?>
 <!DOCTYPE html>
 <html lang="ko">
