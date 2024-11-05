6aa704815f68f90bf112213ef366266bcdf3849e
[dhhan] <[dhhan@enliple.com]>
2024-11-05 17:41:02 +0900

    검증

--- view/header.php
+++ view/header.php
@@ -9,7 +9,21 @@ if ($isTest) {
 
   $apiUrl = 'http://192.168.101.156';
 } else {
+  $userId = $_REQUEST['userId'];
+  $adId = $_REQUEST['adId'];
+  $affliateId = $_REQUEST['affliateId'];
+  $site = $_REQUEST['site'];
+  $zoneId = $_REQUEST['zoneId'];
+
   session_start();
+  if ($userId && $adId && $affliateId && $site && $zoneId) {
+    $_SESSION['check_userId'] = $userId;
+    $_SESSION['check_adId'] = $adId;
+    $_SESSION['check_affliateId'] = $affliateId;
+    $_SESSION['check_site'] = $site;
+    $_SESSION['check_zoneId'] = $zoneId;
+  }
+
   $checkUserId = $_SESSION['check_userId'];
   $checkAdId = $_SESSION['check_adId'];
   $checkAffliateId = $_SESSION['check_affliateId'];
@@ -18,5 +32,14 @@ if ($isTest) {
 
   $adminApiUrl = 'https://admin.shoplus.io';
   $appApiUrl = 'https://app.shoplus.io';
+
+  if (!$checkUserId || !$checkAdId || !$checkAffliateId || !$checkSite || !$checkZoneId) {
+?>
+    <script>
+      alert('필수 값이 없습니다. 종료 후 다시 시도해 주세요.');
+    </script>
+<?
+    exit;
+  }
 }
 ?>
