0c9f687663985d497f165664b416344c6a174639
[dhhan] <[dhhan@enliple.com]>
2024-11-06 12:38:58 +0900

    test

--- view/reward/campaign.php
+++ view/reward/campaign.php
@@ -142,7 +142,7 @@ if (!$object) {
             history.back();
             return;
           }
-          return console.log(buttonUrl);
+          // return console.log(buttonUrl);
           location.href = buttonUrl;
         },
         error: function(request, status, error) {
