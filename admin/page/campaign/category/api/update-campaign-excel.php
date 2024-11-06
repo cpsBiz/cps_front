dac05460414fab186d095bdde29f6dfb5013292e
[dhhan] <[dhhan@enliple.com]>
2024-11-06 16:14:53 +0900

    엑셀

--- admin/page/campaign/category/api/update-campaign-excel.php
+++ admin/page/campaign/category/api/update-campaign-excel.php
@@ -1,9 +1,8 @@
-<? include_once $_SERVER['DOCUMENT_ROOT'] . "/db_config.php"; ?>
-<?
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . "/db_config.php";
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 $response = ['resultCode' => false, 'resultMessage' => ''];
 
@@ -15,16 +14,16 @@ if ($_FILES['excelFile']['error'] == UPLOAD_ERR_OK) {
 
     $worksheet = $spreadsheet->getActiveSheet();
     foreach ($worksheet->getRowIterator(6) as $row) {
-      //캠페인번호
+      // 캠페인번호
       $a = $worksheet->getCell('A' . $row->getRowIndex())->getValue();
-      //카테고리코드
+      // 카테고리코드
       $b = $worksheet->getCell('B' . $row->getRowIndex())->getValue();
-      //매체아이디
+      // 매체아이디
       $c = $worksheet->getCell('C' . $row->getRowIndex())->getValue();
-      //순위
+      // 순위
       $d = $worksheet->getCell('D' . $row->getRowIndex())->getValue();
 
-      // A 열 값이 비어 있으면 더 이상 데이터가 없다고 가정하고 중단
+      // A, B, C, D 값이 비어 있으면 중단
       if (empty($a) || empty($b) || empty($c) || empty($d)) {
         break;
       }
@@ -38,20 +37,24 @@ if ($_FILES['excelFile']['error'] == UPLOAD_ERR_OK) {
               CATEGORY = ?,
               CAMPAIGN_RANK = ?
               ";
+
       $stmt = mysqli_stmt_init($con);
       if (mysqli_stmt_prepare($stmt, $sql)) {
         mysqli_stmt_bind_param($stmt, 'issisi', $a, $b, $c, $d, $b, $d);
-        mysqli_stmt_execute($stmt);
+        if (!mysqli_stmt_execute($stmt)) {
+          throw new Exception('MySQL execute error: ' . mysqli_stmt_error($stmt));
+        }
+      } else {
+        throw new Exception('MySQL prepare error: ' . mysqli_error($con));
       }
     }
 
     $response['resultCode'] = true;
   } catch (Exception $e) {
-    $response['resultMessage'] = '데이터 처리중 문제가 발생했습니다. 다시 시도해 주세요.';
+    $response['resultMessage'] = '데이터 처리중 문제가 발생했습니다. 오류: ' . $e->getMessage();
   }
 } else {
   $response['resultMessage'] = '파일을 재 첨부하여 다시 시도해 주세요.';
 }
 
 echo json_encode($response);
-?>
