1cfb777ec245c6dc4f67e9cc949580d60bf783d6
[dhhan] <[dhhan@enliple.com]>
2024-11-08 13:32:35 +0900

    test

--- view/inquiry/index.php
+++ view/inquiry/index.php
@@ -22,7 +22,7 @@ if (!$campaign) {
   <link rel="stylesheet" href="../css/style.css">
   <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+  <!-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" /> -->
 </head>
 
 <body>
