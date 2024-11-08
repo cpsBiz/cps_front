0f450e26f1dd04bff8ed9afe2fbe9d2b36277eca
[dhhan] <[dhhan@enliple.com]>
2024-11-08 13:20:49 +0900

    test

--- view/inquiry/index.php
+++ view/inquiry/index.php
@@ -20,6 +20,7 @@ if (!$campaign) {
   <title>1:1 문의하기</title>
   <!-- style -->
   <link rel="stylesheet" src="https://app.shoplus.io/css/style.css">
+  <link rel="stylesheet" src="https://app.shoplus.io/view/css/style.css">
   <script type="text/javascript" src="https://admin.shoplus.io/js/lib/moment.min.js"></script>
   <script type="text/javascript" src="https://admin.shoplus.io/js/lib/daterangepicker_popup.js"></script>
   <link type="text/css" rel="stylesheet" src="https://admin.shoplus.io/css/lib/daterangepicker_popup.css" />
