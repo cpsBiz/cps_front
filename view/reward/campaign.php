0cdd728b78eaa89e1906d161e3a4d234975d523d
[dhhan] <[dhhan@enliple.com]>
2024-11-11 10:47:10 +0900

    캐시버전

--- view/reward/campaign.php
+++ view/reward/campaign.php
@@ -15,7 +15,7 @@ if (!$object) {
   <meta name="format-detection" content="telephone=no">
   <title>쇼핑적립 상세정보</title>
   <!-- style -->
-  <link rel="stylesheet" href="/css/style.css?ver=202411061">
+  <link rel="stylesheet" href="/css/style.css?version=<?= $cacheVersion; ?>">
 </head>
 
 <body>
