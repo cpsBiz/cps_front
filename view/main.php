27f0104f61ddcb3dd1ce0ed6dcdf0d4218c806a7
[dhhan] <[dhhan@enliple.com]>
2024-11-14 12:30:33 +0900

    뒤로가기 감지시에만 스크롤 유지

--- view/main.php
+++ view/main.php
@@ -339,7 +339,12 @@
     if (!checkCoupang) removeCoupangArea();
     $('#campaign-list').append(list);
 
-    scrollManager.restore('mainPageScroll');
+    if (window.performance) {
+      const navigation = performance.getEntriesByType('navigation')[0];
+      if (navigation.type === 'back_forward') {
+        scrollManager.restore('mainPageScroll');
+      }
+    }
   }
 
   function renderCoupangArea(item) {
