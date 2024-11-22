522391facab46281dbfefac999abff224fb48bf2
[dhhan] <[dhhan@enliple.com]>
2024-11-22 12:39:48 +0900

    css 수정

--- view/header.php
+++ view/header.php
@@ -20,7 +20,7 @@ if ($isTest) {
   $appApiUrl = 'https://app.shoplus.io';
 }
 
-$cacheVersion = '1.0.1';
+$cacheVersion = '1.0.2';
 ?>
 <link rel="icon" type="image/x-icon" href="<?= $appApiUrl; ?>/images/favicon.ico">
 <link rel="stylesheet" href="<?= $appApiUrl; ?>/css/style.css?version=<?= $cacheVersion; ?>">
