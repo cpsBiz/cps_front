052b568039a15a24ed1752c94275b1d6ab491850
[dhhan] <[dhhan@enliple.com]>
2024-11-26 16:00:40 +0900

    전환율 렌더링

--- admin/page/report/report.php
+++ admin/page/report/report.php
@@ -479,7 +479,7 @@
 		sumRow.appendChild(createCell('th', commaLocale(data.cnt)));
 		sumRow.appendChild(createCell('th', commaLocale(data.clickCnt)));
 		sumRow.appendChild(createCell('th', commaLocale(data.rewardCnt)));
-		sumRow.appendChild(createCell('th', data.rewardRate + '%'));
+		sumRow.appendChild(createCell('th', data.rewardRate === '0.00' ? '0%' : data.rewardRate + '%'));
 		sumRow.appendChild(createCell('th', commaLocale(data.productPrice) + '원'));
 		sumRow.appendChild(createCell('th', commaLocale(data.commission) + '원'));
 		sumRow.appendChild(createCell('th', commaLocale(data.commissionProfit) + '원'));
@@ -514,7 +514,7 @@
 			row.appendChild(createCell('td', commaLocale(item.CNT)));
 			row.appendChild(createCell('td', commaLocale(item.CLICK_CNT)));
 			row.appendChild(createCell('td', commaLocale(item.REWARD_CNT)));
-			row.appendChild(createCell('td', item.REWARD_RATE === '0.00' ? 0 : item.REWARD_RATE + '%'));
+			row.appendChild(createCell('td', item.REWARD_RATE === '0.00' ? '0%' : item.REWARD_RATE + '%'));
 			row.appendChild(createCell('td', commaLocale(item.PRODUCT_PRICE) + '원'));
 			row.appendChild(createCell('td', commaLocale(item.COMMISSION) + '원'));
 			row.appendChild(createCell('td', commaLocale(item.COMMISSION_PROFIT) + '원'));
