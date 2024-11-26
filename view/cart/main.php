b896e892265616f53804aa1537730c7b234f7773
[dhhan] <[dhhan@enliple.com]>
2024-11-26 14:35:29 +0900

    상품등록

--- view/cart/main.php
+++ view/cart/main.php
@@ -1363,7 +1363,7 @@
 
       $.ajax({
         type: 'POST',
-        url: '<?= $appApiUrl; ?>/api/schedule/productLink',
+        url: 'https://admin.shoplus.io/api/schedule/productLink',
         contentType: 'application/json',
         data: JSON.stringify(requestData),
         success: function(result) {
