6eac426ebbfb9866e56d6b77cf10a60651eda47a
한대현 <choco5958@naver.com>
2024-12-04 10:56:13 +0900

    test

--- view/main.php
+++ view/main.php
@@ -7,7 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <title>쇼핑적립</title>
-  <link rel="stylesheet" href="/css/index.css?version=<?= $cacheVersion; ?>">
+  <!-- <link rel="preload" href="/css/index.css?version=<?= $cacheVersion; ?>" as="style">
+  <link rel="stylesheet" href="/css/index.css?version=<?= $cacheVersion; ?>"> -->
 </head>
 
 <body>
@@ -46,7 +47,7 @@
   </div>
 
 </body>
-<script type="text/javascript" src="/js/common.js?version=<?= $cacheVersion; ?>"></script>
+<script defer type="text/javascript" src="/js/common.js?version=<?= $cacheVersion; ?>"></script>
 
 </html>
 <script>
