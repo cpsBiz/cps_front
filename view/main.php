2c1bf9d45eb5332f5feb887ac6b8b6cdf70c80ba
한대현 <choco5958@naver.com>
2024-12-04 10:58:07 +0900

    roll back

--- view/main.php
+++ view/main.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <title>쇼핑적립</title>
-  <!-- <link rel="preload" href="/css/index.css?version=<?= $cacheVersion; ?>" as="style">
-  <link rel="stylesheet" href="/css/index.css?version=<?= $cacheVersion; ?>"> -->
+  <link rel="preload" href="/css/index.css?version=<?= $cacheVersion; ?>" as="style">
+  <link rel="stylesheet" href="/css/index.css?version=<?= $cacheVersion; ?>">
 </head>
 
 <body>
