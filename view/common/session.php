a861e32813ac89a7d296548526759dccc9003780
한대현 <choco5958@naver.com>
2024-12-26 11:10:07 +0900

    test

--- view/common/session.php
+++ view/common/session.php
@@ -28,7 +28,10 @@ if (!$isTest) {
       header('Location:https://app.shoplus.io/cart/detail.php?productCode=' . $productCode . '&optionCode=' . $optionCode . '&merchantId=' . $merchantId);
     }
 ?>
-    <? include_once $_SERVER['DOCUMENT_ROOT'] . '/common/token.php'; ?>
+    <script type="text/javascript" src="https://cdn.shoplus.io/js/lib/jquery-2.2.2.min.js"></script>
+    <script type="text/javascript" src="https://cdn.shoplus.io/js/lib/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="https://cdn.shoplus.io/js/lib/jquery-ui.min.js"></script>
+    <script src="<?= $appApiUrl; ?>/js/common.js"></script>
     <script>
       $(function() {
         <? if ($fcmToken) { ?>
@@ -37,6 +40,51 @@ if (!$isTest) {
           successToken('N');
         <? } ?>
       })
+
+
+      function setToken() {
+        try {
+          const requestData = {
+            userId: '<?= $userId; ?>',
+            affliateId: '<?= $affliateId; ?>',
+            adId: '<?= $adId; ?>',
+            token: '<?= $fcmToken; ?>',
+            os: getOs(),
+            userName: '',
+            userEmail: '',
+            userPhone: ''
+          }
+
+          $.ajax({
+            async: false,
+            type: 'POST',
+            url: 'https://app.shoplus.io/api/common/cpsUser',
+            contentType: 'application/json',
+            data: JSON.stringify(requestData),
+            success: function(result) {
+              if (result.resultCode === '0000') {
+                successToken(result.data.firstLogin);
+              } else {
+                console.log(`${result.resultCode}/${result.resultMessage}`);
+              }
+            },
+            error: function(request, status, error) {
+              console.error(`Error: ${error}`);
+            }
+          });
+        } catch (error) {
+          alert(error);
+        }
+      }
+
+      function successToken(firstLogin) {
+        const host = window.location.hostname;
+        if (host.includes('/cart/index.php') && firstLogin === 'Y') {
+          checkCartAgree();
+        } else {
+          location.replace('https://app.shoplus.io/main.php');
+        }
+      }
     </script>
   <?
   } else {
@@ -54,11 +102,58 @@ if (!$isTest) {
   $affliateId = 'donsee';
   $fcmToken = 'test';
   ?>
-  <? include_once $_SERVER['DOCUMENT_ROOT'] . '/common/token.php'; ?>
+  <script type="text/javascript" src="https://cdn.shoplus.io/js/lib/jquery-2.2.2.min.js"></script>
+  <script type="text/javascript" src="https://cdn.shoplus.io/js/lib/jquery.easing.1.3.js"></script>
+  <script type="text/javascript" src="https://cdn.shoplus.io/js/lib/jquery-ui.min.js"></script>
+  <script src="<?= $appApiUrl; ?>/js/common.js"></script>
   <script>
     $(function() {
       setToken();
     })
+
+    function setToken() {
+      try {
+        const requestData = {
+          userId: '<?= $userId; ?>',
+          affliateId: '<?= $affliateId; ?>',
+          adId: '<?= $adId; ?>',
+          token: '<?= $fcmToken; ?>',
+          os: getOs(),
+          userName: '',
+          userEmail: '',
+          userPhone: ''
+        }
+
+        $.ajax({
+          async: false,
+          type: 'POST',
+          url: 'https://testapp.shoplus.io/api/common/cpsUser',
+          contentType: 'application/json',
+          data: JSON.stringify(requestData),
+          success: function(result) {
+            if (result.resultCode === '0000') {
+              successToken(result.data.firstLogin);
+            } else {
+              console.log(`${result.resultCode}/${result.resultMessage}`);
+            }
+          },
+          error: function(request, status, error) {
+            console.error(`Error: ${error}`);
+          }
+        });
+      } catch (error) {
+        alert(error);
+      }
+    }
+
+    function successToken(firstLogin) {
+      const host = window.location.hostname;
+      if (host.includes('/cart/index.php') && firstLogin === 'Y') {
+        checkCartAgree();
+      } else {
+        location.replace('https://testapp.shoplus.io/main.php');
+      }
+    }
   </script>
 <?
 }
