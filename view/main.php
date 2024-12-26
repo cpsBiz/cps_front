6accc92ed1abf66b4d6cc79fde65884dc52b9a39
한대현 <choco5958@naver.com>
2024-12-26 18:27:49 +0900

    test

--- view/main.php
+++ view/main.php
@@ -455,7 +455,7 @@
   }
 
   function postToUrl(item) {
-    window.open(`/reward/campaign.php?object=${item}`);
+    window.open(`/reward/campaign.php?object=${item}`, '_blank');
   }
 
   function appClose() {
