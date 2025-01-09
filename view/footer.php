ce64d75415c49d94b6365bfee58f65f5c231eae2
한대현 <choco5958@naver.com>
2025-01-09 14:55:22 +0900

    푸터

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,9 @@
-<!-- <div class="bottom-menu-wrap">
-  <a class="menu" href="javascript:void(0)"><span class="ico-cart">카트</span></a>
-  <a class="menu on" href="/index.php"><span class="ico-save">적립</span></a>
-  <a class="menu" href="javascript:void(0)"><span class="ico-trend">트렌드</span></a>
-  <a class="menu" href="javascript:void(0)"><span class="ico-delivery">배송</span></a>
-  <a class="menu" href="/history/point.php"><span class="ico-breakDown">내역</span></a>
-</div> -->
+<? if ($checkUserId === '10000') { ?>
+  <div class="bottom-menu-wrap">
+    <a class="menu" href="<?= $appApiUrl; ?>/cart/main.php"><span class="ico-cart">카트</span></a>
+    <a class="menu" href="<?= $appApiUrl; ?>/main.php"><span class="ico-save">적립</span></a>
+    <!-- <a class="menu" href="javascript:void(0)"><span class="ico-trend">트렌드</span></a>
+  <a class="menu" href="javascript:void(0)"><span class="ico-delivery">배송</span></a> -->
+    <a class="menu" href="<?= $appApiUrl; ?>/history/point.php"><span class="ico-breakDown">내역</span></a>
+  </div>
+<? } ?>
